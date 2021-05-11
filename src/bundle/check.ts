6fce6d08ba1fee677b4384d4092d269a400980c9
lhl2617 <lhlee888@hotmail.com>
2021-05-11 17:14:45 +0100

    Fix bundle:check script and add constants

--- src/bundle/check.ts
+++ src/bundle/check.ts
@@ -3,18 +3,15 @@
 // `npm run bundle:check`
 
 import * as fs from "fs"
-import * as path from "path"
 
-const lyDir = "./ly"
-// https://docs.python.org/3/library/sys.html#sys.platform
-const platforms = ["linux", "macos", "windows"]
-const binName = "python-ly"
+// can't import from '../consts' as that file imports vscode
+const platformPythonLyRelativePathMap = {
+  linux: "ly/dist-linux/python-ly",
+  darwin: "ly/dist-macos/python-ly",
+  win32: "ly/dist-windows/python-ly.exe",
+}
 
-const binPaths = platforms.map((platform) => {
-  return path.join(lyDir, `dist-${platform}`, binName)
-})
-
-binPaths.forEach((binPath) => {
+Object.values(platformPythonLyRelativePathMap).forEach((binPath) => {
   if (!fs.existsSync(binPath)) {
     throw new Error(`"python-ly" bundle incomplete: Missing: ${binPath}`)
   }
---
+++ src/consts.ts
@@ -0,0 +1,15 @@
+import * as vscode from "vscode"
+
+const extensionPublisher = "lhl2617"
+const extensionName = "lilypond-formatter"
+
+export const extensionPath =
+  vscode.extensions.getExtension(`${extensionPublisher}.${extensionName}`)
+    ?.extensionPath ?? ".." // root folder if not found (source code)
+
+// https://nodejs.org/api/process.html#process_process_platform
+export const platformPythonLyRelativePathMap = {
+  linux: "ly/dist-linux/python-ly",
+  darwin: "ly/dist-macos/python-ly",
+  win32: "ly/dist-windows/python-ly.exe",
+}
