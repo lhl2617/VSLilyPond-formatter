b1651d44831448e7fa20a7855e92a6cadeb649d7
lhl2617 <lhlee888@hotmail.com>
2021-05-12 16:20:35 +0100

    Build static binary for linux

--- ly/build.py
+++ ly/build.py
@@ -23,6 +23,11 @@ PYTHON_LY_BIN_PATH = os.path.join(PYTHON_LY_ROOT, "python-ly")
 BUILD_PATH = os.path.join(LY_ROOT, "build")
 DIST_PATH = os.path.join(LY_ROOT, f"dist-{PLATFORM}")
 
+def cmd(x: str):
+    ret = os.system(x)
+    if ret != 0:
+        raise Exception(f"Command '{x}' exited with code {ret}")
+
 if __name__ == "__main__":
     # Clean the build and dist paths if required
     if os.path.exists(BUILD_PATH):
@@ -46,6 +51,23 @@ if __name__ == "__main__":
         ],
         cwd=PYTHON_LY_ROOT,
     )
+    bin_path = (
+        os.path.join(DIST_PATH, "python-ly")
+        if PLATFORM != "windows"
+        else os.path.join(DIST_PATH, "python-ly.exe")
+    )
+
+    # For linux, build a static binary for maximum compatibility
+    # https://gist.github.com/0xhexmex/bbfdb6da7fe560f4aabcc271f4951e43
+    if PLATFORM == "linux":
+        cmd("sudo apt update")
+        cmd("sudo apt install binutils patchelf")
+        cmd("pip install staticx")
+        cmd(f"staticx {bin_path} {bin_path}.static")
+        # Now replace the original binary
+        cmd(f"mv -f {bin_path}.static {bin_path}")
+        # Check static status
+        cmd(f"ldd {bin_path}")
 
     # Clean the created copy of python-ly/python-ly
     os.remove(PYTHON_LY_BIN_PATH)
@@ -53,11 +75,6 @@ if __name__ == "__main__":
     os.remove(os.path.join(PYTHON_LY_ROOT, "python-ly.spec"))
 
     # Set 0755 permissions to built file
-    bin_path = (
-        os.path.join(DIST_PATH, "python-ly")
-        if PLATFORM != "windows"
-        else os.path.join(DIST_PATH, "python-ly.exe")
-    )
     os.chmod(bin_path, 0o755)
 
     print("BUILT PATH:")
